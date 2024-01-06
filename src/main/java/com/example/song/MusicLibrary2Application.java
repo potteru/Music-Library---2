6b2ac8d96b5b1320b7fb54ad15d7744d88fa42d1
adimurthy <potteru2k11@gmail.com>
2024-01-07 04:12:33 +0530

    commit

---
+++ src/main/java/com/example/song/MusicLibrary2Application.java
@@ -0,0 +1,13 @@
+package com.example.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibrary2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibrary2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/song/model/SongRowMapper.java
@@ -0,0 +1,20 @@
+package com.example.song.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class SongRowMapper implements RowMapper<Song> {
+
+	@Override
+	public Song mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		return new Song(rs.getInt("songId"),
+						rs.getString("songName"),
+						rs.getString("lyricist"),
+						rs.getString("singer"),
+						rs.getString("musicDirector"));
+	}
+
+}
